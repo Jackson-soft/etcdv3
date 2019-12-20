49d36562a6be442cf40f183623ce89c5b0b8c88b
fusu <fusu@2dfire.com>
2019-12-20 17:32:04 +0800

    删除compile_commands.json

--- cmake.sh
+++ cmake.sh
@@ -3,13 +3,16 @@ set -u
 
 folder="build"
 
-if [ ! -d "$folder" ]; then
-    mkdir -p $folder
+if [ -d "$folder" ]; then
+    ninja -C $folder -j 6
+    exit 0
 fi
+
+mkdir -p $folder
 
 cmake -B$folder -H. -GNinja -DCMAKE_EXPORT_COMPILE_COMMANDS=YES
 
-ninja -C $folder
+ninja -C $folder -j 6
 
 lnFile="compile_commands.json"
 
